e44b40466250a2266a030086efb239712504a1bb
Muhammad Zafeer <zafeerwains@gmail.com>
2023-03-30 18:15:56 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const id = document.getElementById('jsonIdData');
+// const tbl='<tr><th scope="row" ></th><td>Mark</td><td>Otto</td><td>@mdo</td></tr>'
+fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(res => {
+    for (let i = 0; i < res.products.length; i++) {
+        id.innerHTML += '<tr><td>' + res.products[i].id + '</td><td>' + res.products[i].title + '</td><td>' + res.products[i].description + '</td><td>' + res.products[i].brand + '</td>' + '</td><td>' + res.products[i].category + '</td></tr>';
+    }
+});
+//# sourceMappingURL=index.js.map
